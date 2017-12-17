6b47bcbcf0d13b9ac258a10d29fb5fd174d2b62d
alaa <alaa2amz@gmail.com>
2017-12-17 18:16:56 +0200

    17.12.17.0.18:16-> last of the day

--- VTEdosageTableViewController.swift
+++ VTEdosageTableViewController.swift
@@ -53,7 +53,7 @@ class VTEdosageTableViewController: UITableViewController,UITextFieldDelegate {
         default:
             print("some error entered")
         }
-        let vc = VTEDOSEAlertViewController(result: "\(String(format:"%.1f",dosage)) mg\ndaily", resultComments: "tyuio")
+        let vc = VTEDOSEAlertViewController(result: dosage == 0.6 ? "\(String(format:"%.1f",dosage)) mg/kg/day":"\(String(format:"%.1f",dosage)) mg\ndaily", resultComments: "tyuio")
         self.present(vc, animated: true, completion: nil)
         
     }
